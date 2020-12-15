7acfc13c857a11e8511fa77be0b89a0c6c4e4dc6
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-15 19:20:02 +0700

    Create UrlEncoding.cs

---
+++ Models/UrlEncoding.cs
@@ -0,0 +1,17 @@
+﻿namespace OpenTriviaSharp.Models
+{
+	/// <summary>
+	/// Encoding of JSON response.
+	/// </summary>
+	public enum UrlEncoding
+	{
+		/// <summary>
+		/// All values will be enoded in Base64.
+		/// </summary>
+		Base64, 
+		/// <summary>
+		/// All values will be encoded in URL encoding (RFC 3986).
+		/// </summary>
+		RFC3986
+	}
+}
